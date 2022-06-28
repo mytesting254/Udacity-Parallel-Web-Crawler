435360972175ab785153c69d2fc28ab059e0d675
mytesting254 <83026481+mytesting254@users.noreply.github.com>
2022-06-28 11:07:37 -0400

    First Commit

---
+++ webcrawler/src/main/java/com/udacity/webcrawler/json/ConfigurationLoader.java
@@ -0,0 +1,64 @@
+package com.udacity.webcrawler.json;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
+/**
+ * A static utility class that loads a JSON configuration file.
+ */
+public final class ConfigurationLoader {
+
+  private final Path path;
+
+  /**
+   * Create a {@link ConfigurationLoader} that loads configuration from the given {@link Path}.
+   */
+  public ConfigurationLoader(Path path) {
+    this.path = Objects.requireNonNull(path);
+  }
+
+  /**
+   * Loads configuration from this {@link ConfigurationLoader}'s path
+   *
+   * @return the loaded {@link CrawlerConfiguration}.
+   */
+  public CrawlerConfiguration load() {
+    // TODO: Fill in this method.
+    BufferedReader reader;
+    try {
+      reader = Files.newBufferedReader(this.path);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+    return read(reader);
+  }
+
+  /**
+   * Loads crawler configuration from the given reader.
+   *
+   * @param reader a Reader pointing to a JSON string that contains crawler configuration.
+   * @return a crawler configuration
+   */
+  public static CrawlerConfiguration read(Reader reader) {
+    // This is here to get rid of the unused variable warning.
+   Objects.requireNonNull(reader);
+    // TODO: Fill in this method
+    ObjectMapper mapper = new ObjectMapper();
+
+    try {
+      CrawlerConfiguration.Builder crawler = mapper.readValue(reader, CrawlerConfiguration.Builder.class);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+    return new CrawlerConfiguration.Builder().build();
+  }
+}
